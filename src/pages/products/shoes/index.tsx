1c7e357eb64a6fafd0125f62fecd1abfe2da56cd
git-spec <inkyfischer@gmail.com>
2022-08-19 21:24:22 +0200

    Created api.

---
+++ src/pages/products/shoes/index.tsx
@@ -0,0 +1,57 @@
+/** Next, React */
+import React, { Component } from 'react';
+/** Components */
+import Products from '../../../components/Products/Products';
+/** Interfaces */
+import { IProduct } from '../../../interfaces/IProduct';
+/** Data */
+// import data from '../../../data/products.json';
+
+type Props = {
+    children: JSX.Element;
+    data: IProduct[];
+};
+
+/**
+ * Page of Produktgruppe1
+ * @class Productgruppe1
+ * @extends Component
+ * @return {JSX.Element}
+ */
+export default class Shoes extends Component<Props> {
+    // data: {
+    //     id: number;
+    //     name: string;
+    //     description: string;
+    //     price: number;
+    //     image: string;
+    // }[];
+
+    data: IProduct[];
+    row?: string;
+    col?: string;
+
+    constructor(props: Props) {
+        super(props);
+        this.data = props.data;
+    }
+
+    render() {
+        return (
+            <Products
+                data={this.data.filter((item) => item.category == 'shoes')}
+                row="2rem"
+                col="1rem"
+            />
+        );
+    }
+}
+
+export async function getServerSideProps() {
+    // Fetch data from external API
+    const res = await fetch(`http://localhost:8000/api/products`);
+    const data = await res.json();
+
+    // Pass data to the page via props
+    return { props: { data } };
+}
