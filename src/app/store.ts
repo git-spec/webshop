22508766d5d6df99178f01942911271dafdac8ef
git-spec <inkyfischer@gmail.com>
2022-09-07 16:01:27 +0200

    set redux store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,19 @@
-import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
-import windowSizeReducer from "../features/window/windowSize";
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import cartReducer from '../features/cart/cartSlice';
+// import windowSizeReducer from "../features/window/windowSize";
 
 export const store = configureStore({
     reducer: {
-        windowSize: windowSizeReducer,
+        cart: cartReducer,
+        // windowSize: windowSizeReducer,
     },
 });
 
-export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-    ReturnType,
-    RootState,
-    unknown,
-    Action<string>
->;
+export type AppDispatch = typeof store.dispatch;
+// export type AppThunk<ReturnType = void> = ThunkAction<
+//     ReturnType,
+//     RootState,
+//     unknown,
+//     Action<string>
+// >;
